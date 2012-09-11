42bbc103aa1683451c90c8e6687238a2897a6cc6
Damien Pitard <damien.pitard@gmail.com>
2012-09-11 11:02:37 +0200

    added prod index

---
+++ web/index.php
@@ -0,0 +1,9 @@
+<?php
+
+//umask(0000);  //This will let the permissions be 0777
+
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+$app = require __DIR__.'/../app/app.php';
+$app->run();
